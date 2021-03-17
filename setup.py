aefc137f35e8ba893256fbcfacff93440de75f74
Thiago Piccioni <thiagopiccioni@gmail.com>
2021-03-17 23:08:35 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Fast Api Umbrella',
+    version='1.0.0',
+    description='Service that checks the weather and tells you whether you need a umbrella ar not.',
+    author='Thiago Piccioni',
+    author_email='thiagopiccioni@gmail.com',
+
+    classifiers=[
+        'Development Status :: 2 - Prototype/Not Production Ready',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+)
